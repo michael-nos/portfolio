3db83dd332f7ad7ef3fff7b58e068e4e32a7b27c
michael-nos <155692926+michael-nos@users.noreply.github.com>
2024-01-19 21:41:12 -0500

    using custom css classes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,21 @@
 export const App = () => (
-	<div className="container mx-auto flex flex-col justify-center h-screen items-start">
-		<div className="triangle"></div>
-		<div className="font-semibold text-6xl">Hey,</div>
-		<div className="font-semibold text-6xl">I'm Michael,</div>
-		<div className="font-semibold text-6xl">software engineer</div>
-		<div className="font-extralight text-light-gray mt-3 pb-1 border-b-2 border-b-warm-yellow">React / TypeScript / GraphQL</div>
-		<div className="mt-6">
-			I'm a software engineer with experience in enterprise software development using both monolithic
-			and microservice architecture. I enjoy learning about new technologies to further develop
-			my skillset and to embrace the change in our ever-expanding web ecosystem.
+	<div className="app">
+		<div className="app-corner-triangle"></div>
+		<div className="app-large-heading-text">Hey,</div>
+		<div className="app-large-heading-text">I'm Michael,</div>
+		<div className="app-large-heading-text">software engineer</div>
+		<div className="app-heading-sub-text">React / TypeScript / GraphQL</div>
+		<div className="app-bio-container">
+			<p>
+				I'm a software engineer with experience in enterprise software development using both monolithic
+				and microservice architecture. I enjoy learning about new technologies to further develop
+				my skillset and to embrace the change in our ever-expanding web ecosystem.
+			</p>
 		</div>
-		<div className="mt-3">Want to connect? Feel free to reach out on <a className="text-warm-yellow" href="https://www.linkedin.com/in/michael-nos/" target="_blank">LinkedIn</a>.</div>
+		<div className="app-connect-text-container">
+			<p>
+				Want to connect? Feel free to reach out on <a className="linkedin-link-text" href="https://www.linkedin.com/in/michael-nos/" target="_blank">LinkedIn</a>.
+			</p>
+		</div>
 	</div>
 );
