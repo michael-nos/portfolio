226b175e61282d184f207d10f0ad19ec4ebfbe46
michael-nos <155692926+michael-nos@users.noreply.github.com>
2024-01-19 22:57:38 -0500

    update base url in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: './',
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
