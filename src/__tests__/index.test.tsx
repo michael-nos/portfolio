b4ac594c4f5840320f8ef231c9a7b44cbc1c7367
michael-nos <155692926+michael-nos@users.noreply.github.com>
2024-01-21 11:15:37 -0500

    updated packages and tests

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { App } from '@/components/App';
 
 describe('index', () => {
-  it('should render the mock App component', () => {
+  it('should render the App component', () => {
     render(<App />);
     expect(
       screen.getByText(/React \/ TypeScript \/ GraphQL/i)
